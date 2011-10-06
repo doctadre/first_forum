b9b565043307f2535a5cd45540d4152c16a33b4e
Andre Harris <drdre662@yahoo.com>
2011-10-06 03:59:59 -0700

    fixed bug with users models

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
 	validates_presence_of	:first_name
 	validates_presence_of	:last_name
 	validates	:username,	:uniqueness =>true, 
-							:presence => true
+							:presence => true,
 							:length		=> { :within => 6..40}
 	validates 	:email, :uniqueness =>true, 
 						:presence => true, 
