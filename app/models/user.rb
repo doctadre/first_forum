6aec78ae92fe63f9cea94644c85fc97c9223956f
Andre Harris <drdre662@yahoo.com>
2011-10-07 11:38:16 -0700

    trying to debug user.rb for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 	has_many :posts
 	has_many :topics
 	has_many :forums
-	scope 	 :active, where (:active => true)
+	scope 	 :active, where(:active => true)
 	
 	validates_presence_of	:first_name
 	validates_presence_of	:last_name
